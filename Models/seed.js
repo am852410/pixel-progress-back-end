7fd63515b639d0cb1ae7e150ef9895d2bdd611dd
Antonio Mendoza <antoniomendoza05@gmail.com>
2021-04-18 11:27:52 -0700

    set up CRUD endpoints complete

---
+++ Models/seed.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: "Project 3",
+    completed: false,
+    type: ["school", "bootcamp"],
+    days: ["tuesday", "thursday"],
+    week_start_dates: [
+      new Date("August 19, 2020 23:15:30"),
+      new Date("August 26, 2020 23:15:30")
+    ],
+    categories: ["class", "lab"],
+    user_id: "fdjaknad",
+    completed_tasks: [
+      {
+        day: "tuesday",
+        date: new Date("August 19, 2020 23:15:30"),
+        completed_categories: ["class", "lab"]
+      },
+      {
+        day: "thursday",
+        date: new Date("August 21, 2020 23:15:30"),
+        completed_categories: ["class", "lab"]
+      }
+    ]
+  }
+];
