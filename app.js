4b093d4b69b3dea7064514b6859ef9601f17996c
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-04-22 09:00:59 -0500

    Added PORT const and changed to port 3003 since easier to do on backend and leave 3000 for react app.

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ db.on("error", err => console.log(err.message + " is Mongod not running?"));
 db.on("connected", () => console.log("mongo connected: ", mongoURI));
 db.on("disconnected", () => console.log("mongo disconnected"));
 
-// Goal.create(goalSeed, (err, data) => {
-//   if (err) console.log(err.message);
-//   console.log("added provided goal data");
-// });
+Goal.create(goalSeed, (err, data) => {
+  if (err) console.log(err.message);
+  console.log("added provided goal data");
+});
 Goal.count({}, (err, data) => {
   if (err) console.log(err.message);
   console.log(`There are ${data} goals in this database`);
